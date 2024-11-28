524a9713718f7d17173e7666e2fef7b922cb8445
winston <winston.los.santos@gmail.com>
2024-11-28 18:52:32 +0800

    Register category command

--- cli.js
+++ cli.js
@@ -4,11 +4,13 @@ const { Command } = require("commander");
 const pull = require("./commands/pull.command");
 const price = require("./commands/price.command");
 const mcap = require("./commands/mcap.command");
+const category = require("./commands/category.command");
 
 const program = new Command();
 
 pull.register(program);
 price.register(program);
 mcap.register(program);
+category.register(program);
 
 program.parse(process.argv);
